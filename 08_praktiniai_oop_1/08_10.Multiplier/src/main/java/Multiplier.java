64d784e85557889011660f85541b089abeffe0f9
Stalkakuma <stalkakuma@gmail.com>
2024-09-16 14:47:33 +0300

    completed first OOP task_1

---
+++ 08_praktiniai_oop_1/08_10.Multiplier/src/main/java/Multiplier.java
@@ -0,0 +1,11 @@
+public class Multiplier {
+    private int factor;
+
+    public Multiplier(int number) {
+        this.factor = number;
+    }
+
+    public int multiply(int number) {
+        return number *= this.factor;
+    }
+}
